d1aeec41e33a1f522e34a34d4d3e5fcc84a97c17
deepaklearner <deepaklearner@gmail.com>
2024-06-09 16:53:23 -0400

    updated v2

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,22 @@ from app import app
 @app.route('/index.html')
 def index():
     user = {"username": "deepak"}
-    return render_template('index.html', title='Home', username=user['username'])
+    posts =[
+        {
+            "author": {"username": "deepak"},
+            "body"  : "what a sunny day"
+        },
+        {
+            "author": {"username": "ankita"},
+            "body"  : "what a nice day"
+        },
+        {
+            "author": {"username": "harmesh"},
+            "body"  : "what a nice bowl by Boomra"
+        },
+        {
+            "author": {"username": "nivedita"},
+            "body"  : "Hey guyzzz"
+        }
+    ]
+    return render_template('index.html', title='Home', username=user['username'], posts=posts)
